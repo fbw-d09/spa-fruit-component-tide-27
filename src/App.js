3d90b78673c87e68658105eaff7a9f55cb14f0ca
tide-27 <tianzen98@gmail.com>
2023-06-20 15:47:30 +0200

    fertig

--- src/App.js
+++ src/App.js
@@ -1,22 +1,80 @@
-import React, {useState} from 'react'
-import './App.css';
-import Fruit from './components/Fruit' ;
-function App() {
-  //Add your code here
+// import React, {useState} from 'react'
+// import './App.css';
+// import Fruit from './components/Fruit' ;
+// function App() {
+//   //Add your code here
 
+//   return (
+//     <div className="App">
+//      {/* add you code here */}
 
+//     </div>
+//   );
+// }
 
+// export default App;
 
+import React, { useState } from "react";
+import Fruit from "./components/Fruit";
 
+const App = () => {
+  const initialFruits = [
+    { id: 1, name: "Apple", color: "red" },
+    { id: 2, name: "Banana", color: "blue" },
+    { id: 3, name: "Orange", color: "green" },
+    { id: 4, name: "Grapes", color: "yellow" },
+  ];
+
+  const [fruits, setFruits] = useState(initialFruits);
+  const [deletedFruits, setDeletedFruits] = useState([]);
+
+  function changeFruitColor(id, color) {
+    const updatedFruits = fruits.map((fruit) =>
+      fruit.id === id ? { ...fruit, color } : fruit
+    );
+    setFruits(updatedFruits);
+  }
+
+  function deleteFruit(id) {
+    const deletedFruit = fruits.find((fruit) => fruit.id === id);
+    const updatedFruits = fruits.filter((fruit) => fruit.id !== id);
+    setFruits(updatedFruits);
+    setDeletedFruits([...deletedFruits, deletedFruit]);
+  }
+
+  function restoreFruits() {
+    setFruits([...fruits, ...deletedFruits]);
+    setDeletedFruits([]);
+  }
+
+  function deleteAllFruits() {
+    setFruits([]);
+    setDeletedFruits([]);
+  }
 
   return (
-    <div className="App">
-     {/* add you code here */}
-
-     
-       
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      {fruits.map((fruit) => (
+        <div key={fruit.id} style={{ marginBottom: "10px" }}>
+          <Fruit
+            id={fruit.id}
+            name={fruit.name}
+            color={fruit.color}
+            onColorChange={changeFruitColor}
+            onDelete={deleteFruit}
+          />
+        </div>
+      ))}
+      {deletedFruits.length > 0 && (
+        <div style={{ marginTop: "10px" }}>
+          <button onClick={restoreFruits}>Restore All Fruits</button>
+        </div>
+      )}
+      <button onClick={deleteAllFruits}>Delete All</button>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Fruit.js
+++ src/components/Fruit.js
@@ -1,22 +1,49 @@
-import React, {useState} from 'react'
+// import React, {useState} from 'react'
 
-function Fruit() {
-   ////////Add you Code here 
-      
-    
+// function Fruit() {
+//    ////////Add you Code here
 
+//     return (
+//         <div data-testid="fruit">
+//             {/* add your code here */}
 
-    return (
-        <div data-testid="fruit">
-            {/* add your code here */}
+//         </div>
+//     )
+// }
 
+// export default Fruit
 
+import React, { useState } from "react";
 
+const colors = ["red", "blue", "green", "yellow", "purple"];
 
+const Fruit = ({ id, name, color, onColorChange, onDelete }) => {
+  const [textColor, setTextColor] = useState(color);
 
-            
-        </div>
-    )
-}
+  function getRandomColor() {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  }
 
-export default Fruit
+  function changeColor() {
+    const newColor = getRandomColor();
+    setTextColor(newColor);
+    onColorChange(id, newColor);
+  }
+
+  function handleDelete() {
+    onDelete(id);
+  }
+
+  return (
+    <div style={{ color: textColor, textAlign: "center" }}>
+      <p>{name}</p>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button onClick={changeColor}>Change Color</button>
+        <button onClick={handleDelete}>Delete Fruit</button>
+      </div>
+    </div>
+  );
+};
+
+export default Fruit;
